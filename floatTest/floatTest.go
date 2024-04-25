a3b8ffef9b459feb8b368ee0d66c4c8525064477
Sudharsan Ravikumar <supersudh@gmail.com>
2024-04-25 21:41:43 +0100

    function calls and if else statements

--- floatTest/floatTest.go
+++ floatTest/floatTest.go
@@ -1,8 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func main() {
-	var floatNum float64 = 289734789237423.23
-	fmt.Println(floatNum)
+func floatTest() {
+	var printValue string = "Hello world"
+	printMe(printValue)
+
+	var numerator int = 11
+	var denominator int = 0
+	var result, remainder, err = intDivision(numerator, denominator)
+	if err != nil {
+		fmt.Printf(err.Error())
+	} else if remainder == 0 {
+		fmt.Printf("The result of the integer division is %v", result)
+	} else {
+		fmt.Printf("The result of the integer division is %v with remainder %v", result, remainder)
+	}
+	fmt.Printf("The result of the integer division is %v with remainder %v", result, remainder)
+}
+
+func printMe(str string) {
+	fmt.Println(str)
+}
+
+func intDivision(numerator int, denominator int) (int, int, error) {
+	var err error
+	if denominator == 0 {
+		err = errors.New("Cannot divide by zero")
+		return 0, 0, err
+	}
+	var result int = numerator / denominator
+	var remainer int = numerator % denominator
+	return result, remainer, err
 }
