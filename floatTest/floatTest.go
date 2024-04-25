4219664092396418b35c33275ca0259c7c42faeb
Sudharsan Ravikumar <supersudh@gmail.com>
2024-04-25 11:28:47 +0100

    floating point

---
+++ floatTest/floatTest.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var floatNum float64 = 289734789237423.23
+	fmt.Println(floatNum)
+}
---
+++ integerTest/integerTest[IntTest].go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var intNum int = 32767
+	intNum = intNum + 1
+	fmt.Println(intNum)
+}
